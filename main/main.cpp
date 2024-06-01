78cfd13b8aee9e29c5f029b47394ee555ab7e274
Patrick Bertsch <47899331+pat-bert@users.noreply.github.com>
2024-06-01 20:55:41 +0200

    Working example with lvgl and esp-idf update Previously DMA was limiting below the frame size (chip select not kept low between transmission chunks) Lvgl only transmit a part of the frame buffer so no artifacts, nothing lost

--- main/main.cpp
+++ main/main.cpp
@@ -6,40 +6,154 @@
 #include "esp_lcd_panel_ops.h"
 
 #include "esp_log.h"
+#include "esp_timer.h"
 #include "rom/ets_sys.h"
 
 #include "bmp280/bmp2.h"
 #include "st7735/esp_lcd_panel_custom_vendor.h"
-// #include "esp_lcd_panel_vendor.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#include "lvgl.h"
 
 // I2C common protocol defines
 #define ACK_CHECK_EN true   /*!< I2C master will check ack from slave*/
 #define ACK_CHECK_DIS false /*!< I2C master will not check ack from slave */
 
+// I2C Pins
 #define I2C_SDA_PIN GPIO_NUM_21
 #define I2C_SCL_PIN GPIO_NUM_22
 #define BMP_I2C_PORT I2C_NUM_0
+
+// BMP280 Settings
 #define BMP2_32BIT_COMPENSATION
 
-#define LCD_PIN_NUM_SCLK GPIO_NUM_18
-#define LCD_PIN_NUM_MOSI GPIO_NUM_23
-#define LCD_PIN_NUM_CS GPIO_NUM_25
-#define LCD_PIN_NUM_DC GPIO_NUM_26
-#define LCD_PIN_NUM_RST GPIO_NUM_27
+// SPI Settings
+#define LCD_HOST VSPI_HOST
+#define LCD_SPI_CLOCK_HZ (40 * 1000 * 1000)
+#define LCD_SPI_TRADITIONAL_MODE (0) // CPOL = 0, CPHA = 0
 
-#define LCD_V_RES 128
-#define LCD_H_RES 160
-#define LCD_PARALLEL_LINES 80
-#define LCD_PIXEL_CLOCK_HZ (20 * 1000 * 1000)
+#define PIN_NUM_LCD_SCLK GPIO_NUM_18
+#define PIN_NUM_LCD_MOSI GPIO_NUM_23
+#define PIN_NUM_LCD_CS GPIO_NUM_5
+#define PIN_NUM_LCD_DC GPIO_NUM_26
+#define PIN_NUM_LCD_RST GPIO_NUM_27
+
+// ST7735 Settings
+#define LCD_H_RES (160)
+#define LCD_V_RES (128)
 #define LCD_CMD_BITS 8
 #define LCD_PARAM_BITS 8
-#define LCD_HOST SPI3_HOST
 
+// LVGL Settings
+#define LVGL_TICK_PERIOD_MS 2
+#define LVGL_TASK_MAX_DELAY_MS 500
+#define LVGL_TASK_MIN_DELAY_MS 1
+#define LVGL_TASK_STACK_SIZE (4 * 1024)
+#define LVGL_TASK_PRIORITY 2
+
+// FreeRTOS
 #define STACK_SIZE_SENSOR_TASK 2048
 #define STACK_SIZE_DISPLAY_TASK 4096
+
+static SemaphoreHandle_t lvgl_mux = NULL;
+
+static lv_obj_t *meter;
+
+static void set_value(void *indic, int32_t v)
+{
+    lv_meter_set_indicator_end_value(meter, (lv_meter_indicator_t *)indic, v);
+}
+
+void example_lvgl_demo_ui(lv_disp_t *disp)
+{
+    lv_obj_t *scr = lv_disp_get_scr_act(disp);
+    meter = lv_meter_create(scr);
+    lv_obj_center(meter);
+    lv_obj_set_size(meter, 120, 120);
+
+    /*Add a scale first*/
+    lv_meter_scale_t *scale = lv_meter_add_scale(meter);
+    lv_meter_set_scale_ticks(meter, scale, 41, 2, 5, lv_palette_main(LV_PALETTE_GREY));
+    lv_meter_set_scale_major_ticks(meter, scale, 8, 4, 10, lv_color_black(), 10);
+
+    lv_meter_indicator_t *indic;
+
+    /*Add a blue arc to the start*/
+    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_BLUE), 0);
+    lv_meter_set_indicator_start_value(meter, indic, 0);
+    lv_meter_set_indicator_end_value(meter, indic, 20);
+
+    /*Make the tick lines blue at the start of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_BLUE), false, 0);
+    lv_meter_set_indicator_start_value(meter, indic, 0);
+    lv_meter_set_indicator_end_value(meter, indic, 20);
+
+    /*Add a red arc to the end*/
+    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_meter_set_indicator_start_value(meter, indic, 80);
+    lv_meter_set_indicator_end_value(meter, indic, 100);
+
+    /*Make the tick lines red at the end of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED), false, 0);
+    lv_meter_set_indicator_start_value(meter, indic, 80);
+    lv_meter_set_indicator_end_value(meter, indic, 100);
+
+    /*Add a needle line indicator*/
+    indic = lv_meter_add_needle_line(meter, scale, 4, lv_palette_main(LV_PALETTE_GREY), -10);
+
+    /*Create an animation to set the value*/
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_exec_cb(&a, set_value);
+    lv_anim_set_var(&a, indic);
+    lv_anim_set_values(&a, 0, 100);
+    lv_anim_set_time(&a, 2000);
+    lv_anim_set_repeat_delay(&a, 100);
+    lv_anim_set_playback_time(&a, 500);
+    lv_anim_set_playback_delay(&a, 100);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_start(&a);
+}
+
+static bool notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_disp_drv_t *disp_driver = (lv_disp_drv_t *)user_ctx;
+    lv_disp_flush_ready(disp_driver);
+    return false;
+}
+
+static void lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map)
+{
+    esp_lcd_panel_handle_t panel_handle = (esp_lcd_panel_handle_t)drv->user_data;
+    int offsetx1 = area->x1;
+    int offsetx2 = area->x2;
+    int offsety1 = area->y1;
+    int offsety2 = area->y2;
+    // copy a buffer's content to a specific area of the display
+    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, color_map);
+}
+
+static void increase_lvgl_tick(void *arg)
+{
+    /* Tell LVGL how many milliseconds has elapsed */
+    lv_tick_inc(LVGL_TICK_PERIOD_MS);
+}
+
+bool lvgl_lock(int timeout_ms)
+{
+    // Convert timeout in milliseconds to FreeRTOS ticks
+    // If `timeout_ms` is set to -1, the program will block until the condition is met
+    const TickType_t timeout_ticks = (timeout_ms == -1) ? portMAX_DELAY : pdMS_TO_TICKS(timeout_ms);
+    return xSemaphoreTakeRecursive(lvgl_mux, timeout_ticks) == pdTRUE;
+}
+
+void lvgl_unlock(void)
+{
+    xSemaphoreGiveRecursive(lvgl_mux);
+}
 
 struct I2CInterfaceData
 {
@@ -82,14 +196,52 @@ void initI2C()
 void initSPI()
 {
     spi_bus_config_t buscfg{};
-    buscfg.sclk_io_num = LCD_PIN_NUM_SCLK;
-    buscfg.mosi_io_num = LCD_PIN_NUM_MOSI;
+    buscfg.sclk_io_num = PIN_NUM_LCD_SCLK;
+    buscfg.mosi_io_num = PIN_NUM_LCD_MOSI;
     buscfg.miso_io_num = -1;
-    buscfg.quadwp_io_num = -1;                                         // Quad SPI LCD driver is not yet supported
-    buscfg.quadhd_io_num = -1;                                         // Quad SPI LCD driver is not yet supported
-    buscfg.max_transfer_sz = LCD_H_RES * LCD_V_RES * sizeof(uint16_t); // transfer 80 lines of pixels (assume pixel is RGB565) at most in one SPI transaction
+    buscfg.quadwp_io_num = -1;                                  // Quad SPI LCD driver is not yet supported
+    buscfg.quadhd_io_num = -1;                                  // Quad SPI LCD driver is not yet supported
+    buscfg.max_transfer_sz = LCD_H_RES * 80 * sizeof(uint16_t); // transfer 80 lines of pixels (assume pixel is RGB565) at most in one SPI transaction
 
     ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO)); // Enable the DMA feature
+}
+
+esp_lcd_panel_io_handle_t initPanelIO(lv_disp_drv_t *disp_drv)
+{
+    initSPI();
+
+    esp_lcd_panel_io_handle_t io_handle = nullptr;
+    esp_lcd_panel_io_spi_config_t io_config{};
+    io_config.dc_gpio_num = PIN_NUM_LCD_DC;
+    io_config.cs_gpio_num = PIN_NUM_LCD_CS;
+    io_config.pclk_hz = LCD_SPI_CLOCK_HZ;
+    io_config.lcd_cmd_bits = LCD_CMD_BITS;
+    io_config.lcd_param_bits = LCD_PARAM_BITS;
+    io_config.spi_mode = LCD_SPI_TRADITIONAL_MODE;
+    io_config.flags.sio_mode = 1; // only MOSI, no MISO
+    io_config.trans_queue_depth = 10;
+    io_config.on_color_trans_done = notify_lvgl_flush_ready;
+    io_config.user_ctx = disp_drv;
+
+    // Attach the LCD to the SPI bus
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+
+    return io_handle;
+}
+
+esp_lcd_panel_handle_t initPanel(lv_disp_drv_t *disp_drv)
+{
+    esp_lcd_panel_io_handle_t io_handle = initPanelIO(disp_drv);
+    esp_lcd_panel_handle_t panel_handle = nullptr;
+    esp_lcd_panel_dev_config_t panel_config{};
+    panel_config.reset_gpio_num = PIN_NUM_LCD_RST;
+    panel_config.rgb_endian = LCD_RGB_ENDIAN_RGB;
+    panel_config.bits_per_pixel = 16;
+
+    // Create LCD panel handle for ST7735, with the SPI IO device handle
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7735(io_handle, &panel_config, &panel_handle));
+
+    return panel_handle;
 }
 
 BMP2_INTF_RET_TYPE bmp2_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, const void *intf_ptr)
@@ -243,52 +395,29 @@ void task_sensor(void *pvParameters)
     }
 }
 
-void task_display(void *pvParameters)
+static void lvgl_port_task(void *arg)
 {
-    initSPI();
-
-    esp_lcd_panel_io_handle_t io_handle = nullptr;
-    esp_lcd_panel_io_spi_config_t io_config{};
-    io_config.dc_gpio_num = LCD_PIN_NUM_DC;
-    io_config.cs_gpio_num = LCD_PIN_NUM_CS;
-    io_config.pclk_hz = LCD_PIXEL_CLOCK_HZ;
-    io_config.lcd_cmd_bits = LCD_CMD_BITS;
-    io_config.lcd_param_bits = LCD_PARAM_BITS;
-    io_config.spi_mode = 0;       // CPOL = CPHA = 0
-    io_config.flags.sio_mode = 1; // only MOSI, no MISO
-    io_config.trans_queue_depth = 10;
-
-    // Attach the LCD to the SPI bus
-    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
-
-    esp_lcd_panel_handle_t panel_handle = nullptr;
-    esp_lcd_panel_dev_config_t panel_config{};
-    panel_config.reset_gpio_num = LCD_PIN_NUM_RST;
-    panel_config.rgb_endian = LCD_RGB_ENDIAN_RGB;
-    panel_config.bits_per_pixel = 16;
-
-    // Create LCD panel handle for ST7735, with the SPI IO device handle
-    ESP_ERROR_CHECK(esp_lcd_new_panel_st7735(io_handle, &panel_config, &panel_handle));
-
-    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
-    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
-    vTaskDelay(pdMS_TO_TICKS(100));
-
-    uint16_t *colorData = (uint16_t *)heap_caps_malloc(LCD_H_RES * LCD_V_RES * sizeof(uint16_t), MALLOC_CAP_DMA);
-    for (int i = 0; i < LCD_H_RES; ++i)
-    {
-        for (int j = 0; j < LCD_V_RES; ++j)
-        {
-            colorData[i * LCD_H_RES + j] = swapBytes(RGB888ToRGB565(255, 0, 0));
-        }
-    }
-
-    esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, LCD_H_RES, LCD_V_RES, (void *)&colorData);
+    ESP_LOGI("LVGL", "Starting LVGL task");
+    uint32_t task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
 
     while (true)
     {
-        vTaskDelay(pdMS_TO_TICKS(20));
+        // Lock the mutex due to the LVGL APIs are not thread-safe
+        if (lvgl_lock(-1))
+        {
+            task_delay_ms = lv_timer_handler();
+            // Release the mutex
+            lvgl_unlock();
+        }
+        if (task_delay_ms > LVGL_TASK_MAX_DELAY_MS)
+        {
+            task_delay_ms = LVGL_TASK_MAX_DELAY_MS;
+        }
+        else if (task_delay_ms < LVGL_TASK_MIN_DELAY_MS)
+        {
+            task_delay_ms = LVGL_TASK_MIN_DELAY_MS;
+        }
+        vTaskDelay(pdMS_TO_TICKS(task_delay_ms));
     }
 }
 
@@ -298,9 +427,66 @@ extern "C" void app_main(void)
     xTaskCreate(task_sensor, "Sensor", STACK_SIZE_SENSOR_TASK, nullptr, tskIDLE_PRIORITY + 1, &xHandleSensor);
     configASSERT(xHandleSensor);
 
+    static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
+    static lv_disp_drv_t disp_drv;      // contains callback functions
+
+    ESP_LOGI("MAIN", "Install ST7735 panel driver");
+    esp_lcd_panel_handle_t panel_handle = initPanel(&disp_drv);
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, true, false));
+    ESP_ERROR_CHECK(esp_lcd_panel_swap_xy(panel_handle, true));
+
+    // user can flush pre-defined pattern to the screen before we turn on the screen or backlight
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    ESP_LOGI("MAIN", "Initialize LVGL library");
+    lv_init();
+    // alloc draw buffers used by LVGL
+    // it's recommended to choose the size of the draw buffer(s) to be at least 1/10 screen sized
+    lv_color_t *buf1 = (lv_color_t *)heap_caps_malloc(LCD_H_RES * 20 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf1);
+    lv_color_t *buf2 = (lv_color_t *)heap_caps_malloc(LCD_H_RES * 20 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+    assert(buf2);
+    // initialize LVGL draw buffers
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, LCD_H_RES * 20);
+
+    ESP_LOGI("MAIN", "Register display driver to LVGL");
+    lv_disp_drv_init(&disp_drv);
+    disp_drv.hor_res = LCD_H_RES;
+    disp_drv.ver_res = LCD_V_RES;
+    disp_drv.flush_cb = lvgl_flush_cb;
+    disp_drv.draw_buf = &disp_buf;
+    disp_drv.user_data = panel_handle;
+    lv_disp_t *disp = lv_disp_drv_register(&disp_drv);
+
+    ESP_LOGI("MAIN", "Install LVGL tick timer");
+    // Tick interface for LVGL (using esp_timer to generate 2ms periodic event)
+    esp_timer_create_args_t lvgl_tick_timer_args{};
+    lvgl_tick_timer_args.callback = &increase_lvgl_tick;
+    lvgl_tick_timer_args.name = "lvgl_tick";
+
+    esp_timer_handle_t lvgl_tick_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, LVGL_TICK_PERIOD_MS * 1000));
+
+    lvgl_mux = xSemaphoreCreateRecursiveMutex();
+    assert(lvgl_mux);
+
+    ESP_LOGI("MAIN", "Create LVGL task");
     TaskHandle_t xHandleDisplay = nullptr;
-    xTaskCreate(task_display, "Display", STACK_SIZE_DISPLAY_TASK, nullptr, tskIDLE_PRIORITY + 1, &xHandleDisplay);
+    xTaskCreate(lvgl_port_task, "Display", LVGL_TASK_STACK_SIZE, nullptr, LVGL_TASK_PRIORITY, &xHandleDisplay);
     configASSERT(xHandleDisplay);
+
+    ESP_LOGI("MAIN", "Display LVGL Meter Widget");
+    // Lock the mutex due to the LVGL APIs are not thread-safe
+    if (lvgl_lock(-1))
+    {
+        example_lvgl_demo_ui(disp);
+        // Release the mutex
+        lvgl_unlock();
+    }
 
     vTaskSuspend(nullptr);
 }
